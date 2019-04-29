59fdd5bc1775cc4f8306f9f55d9cb156cc7612a7
tanshaobo <18235140339@163.com>
2019-04-29 11:09:19 +0800

    安卓下载完成

--- public_html/static/js/script.js
+++ public_html/static/js/script.js
@@ -1,3 +1,14 @@
-
-
-
+let btnDownLoad = document.querySelector('.btn-download')
+let isWeChat = document.querySelector('.is-wechat')
+btnDownLoad.onclick = function () {
+  let u = navigator.userAgent
+  let WeChatReg = u.indexOf('MicroMessenger') > -1
+  if (WeChatReg) {
+    isWeChat.style.display = 'block'
+  } else {
+    window.location.href = 'http://upld.17k.ren/common/apk/kuaidu_3_3_7_web_0422_1.apk'
+  }
+}
+isWeChat.onclick = function () {
+  isWeChat.style.display = 'none'
+}
